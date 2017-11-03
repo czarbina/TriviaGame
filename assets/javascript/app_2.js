dab1d95ddaf0c9dbb11e4566be5990cc6a3508ca
Sabrina Kesler <sabrina@getvictorious.com>
2017-11-02 20:10:37 -0700

    Hid main question section after hitting results button. Now, time for logic.

--- assets/javascript/app_2.js
+++ assets/javascript/app_2.js
@@ -1,9 +1,29 @@
 
 $(document).ready(function(){
 
+	var countdownTimer = 60;    
+    var intervalId;
+
 	$("#startButton").on("click", function(){
    		$("#mainSection").css("display","block");
    		$("#start").hide();
+
+   		function start() {
+      		intervalId = setInterval(decrement, 1000);
+    
+    		function decrement() {
+	    		countdownTimer--;
+	    		$("span").text(" " + countdownTimer); 
+	    		if (countdownTimer === 0) {
+	    		stop();
+    			}
+    		}	
+		}
+	});	
+
+	$("#doneBtn").on("click", function(){
+		$("#results").css("display","block")
+		$("#mainSection").hide();
 	});
 
 });
