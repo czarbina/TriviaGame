0ff9908437e9b46e23650d98776d042c127e4514
Sabrina Kesler <sabrina@getvictorious.com>
2017-11-02 21:44:02 -0700

    So... close... I can feel it...

--- assets/javascript/app_2.js
+++ assets/javascript/app_2.js
@@ -1,12 +1,77 @@
 
 $(document).ready(function(){
 
-	var countdownTimer = 5;    
+	var countdownTimer = 60;    
     var intervalId;
 
     function stop() {
     	clearInterval(intervalId);
     }
+
+    function grade() {
+    	var totalQs = 5;
+    	var correctAs = 0;
+    //May update or exclude below var 
+    	var alertText;
+    	var i;
+
+    	var a1 = document.getElementsByName("q1");
+    		for (i = 0; i < a1.length; i++) {
+    			if (a1[i].checked) {
+    				if(a1[i].value == "1") {
+    					correctAs++;
+    					break;
+    				}
+    			}
+    		}
+
+    	var a2 = document.getElementsByName("q2");
+    		for (i = 0; i < a2.length; i++) {
+    			if (a2[i].checked) {
+    				if(a2[i].value == "2") {
+    					correctAs++;
+    					break;
+    				}
+    			}
+    		}
+    		
+    	var a3 = document.getElementsByName("q3");
+    		for (i = 0; i < a1.length; i++) {
+    			if (a3[i].checked) {
+    				if(a3[i].value == "3") {
+    					correctAs++;
+    					break;
+    				}
+    			}
+    		}
+    	var a4 = document.getElementsByName("q4");
+    		for (i = 0; i < a4.length; i++) {
+    			if (a4[i].checked) {
+    				if(a4[i].value == "4") {
+    					correctAs++;
+    					break;
+    				}
+    			}
+    		}
+
+    	var a5 = document.getElementsByName("q5");
+    		for (i = 0; i < a5.length; i++) {
+    			if (a5[i].checked) {
+    				if(a5[i].value == "1") {
+    					correctAs++;
+    					break;
+    				}
+    			}
+    		}	
+    	if(correctAs == totalQs) {
+    		console.log("Nailed it.");
+    	}
+
+    	else {
+    		console.log("You got " + correctAs + " out of" + totalQs + " correct!");		
+    	}		
+
+    }	
 
 	$("#startButton").on("click", start);
 
@@ -25,7 +90,7 @@ $(document).ready(function(){
 			$("#mainSection").hide();
     		}
     	}	
-	}	
+	}		
 
 	$("#doneBtn").on("click", function(){
 		stop();
@@ -33,5 +98,10 @@ $(document).ready(function(){
 		$("#mainSection").hide();
 
 	});
+
 });	
+
+
+
+
 
